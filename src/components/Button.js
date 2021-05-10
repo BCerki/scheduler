7edaf9a1ddcd7db8b18a7b0f14e681c56358ac59
unknown <briannac@shaw.ca>
2021-05-10 23:48:08 +0100

    create button according to Building a Button exercise

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+
+  return (
+    <button
+      onClick={props.onClick}
+      className={buttonClass}
+      disabled={props.disabled}>
+      {props.children}
+    </button>
+  );
 }
