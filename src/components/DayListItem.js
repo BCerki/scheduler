ead9abb2578632e7fc22a162c237ef234f693acb
unknown <briannac@shaw.ca>
2021-05-11 01:13:31 +0100

    test the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,13 +7,33 @@ import { action } from "@storybook/addon-actions/dist/preview";
 export default function DayListItem(props) {
   let dayClass = classNames("day-list__item", { "day-list__item--selected": props.selected }, { "day-list__item--full": props.spots === 0 });
   console.log(dayClass)
+
+  const formatSpots = function (spots) {
+    let string = "remaining";
+    if (spots > 1) {
+      string = `${spots} spots ${string}`
+    }
+    if (spots === 1) {
+      string = `${spots} spot ${string}`
+    }
+    if (spots === 0) {
+      string = `no spots ${string}`
+    }
+
+    return (
+      <>
+        {string}
+      </>
+    )
+  }
+
   return (
 
     <li
       className={dayClass}
       onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
