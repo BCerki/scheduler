0cdb17d3c6d35384590c547fa11d81af707828b8
unknown <briannac@shaw.ca>
2021-05-12 05:05:28 +0100

    add appointment views to Application.js per The Schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,52 @@
 import React, { useState } from "react";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
+
 
 import "components/Application.scss";
+
+//Test Data
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 const days = [
   {
@@ -50,8 +94,14 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        < DayListItem />
+        {appointments.map(appointment => {
+          return <Appointment
+            key={appointment.id}
+            {...appointment}
+          />
+        })
+        }
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
