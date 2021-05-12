8c37f84937b7f2a8a60e382bee4e81bc4d010f64
unknown <briannac@shaw.ca>
2021-05-12 23:24:16 +0100

    handle state with a single object per Managing State

--- src/components/Application.js
+++ src/components/Application.js
@@ -53,14 +53,25 @@ const appointments = [
 
 export default function Application() {
 
-  const [day, setDay] = useState([]);
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState([]);
+  // const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // appointments: {}
+  })
+
+  //functions to update state
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
 
   useEffect(() => {
     axios.get('/api/days')
       .then(response => {
         console.log(response.data)
-        setDays([...response.data])
+        setDays(response.data)
       });
   }, [])
 
@@ -77,8 +88,8 @@ export default function Application() {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay} //this also seemed to work? setDay={day => setDay(day)}
           />
 
