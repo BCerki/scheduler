11ee1a6fb881ec93b90226db0b2a1665405d0a1e
unknown <briannac@shaw.ca>
2021-05-14 00:11:43 +0100

    complete Conditionally Rendering our First Components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,32 @@ import "components/Appointment/styles.scss"
 import Header from "components/Appointment/Header"
 import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
+import useVisualMode from "../../hooks/useVisualMode"
 
 export default function Appointment(props) {
+
+
+  //mode constants
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+
+
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY)
+
+
   return (
     <>
       <Header time={props.time} />
       <article className="appointment">
-        {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+        {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        )}
+
       </article>
     </>)
 }
