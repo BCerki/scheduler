76ce5302cb839f0b120e50f4a9e01cde78a6a46c
unknown <briannac@shaw.ca>
2021-05-15 02:23:43 +0100

    create updateSpots function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,8 +1,10 @@
+//FIXFIX if you don't selevt interterviewer, error
+
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const useApplicationHook = function () {
-
+  //State updates
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -10,8 +12,6 @@ const useApplicationHook = function () {
     interviewers: {}
   })
   const setDay = day => setState({ ...state, day });
-
-
 
   //Custom hook
   useEffect(() => {
@@ -31,6 +31,54 @@ const useApplicationHook = function () {
       });
   }, [])
 
+
+  //spots updating
+  const updateSpots = function (id) {
+    let dayName = null;
+    let apptArray = [];
+    let spotsArray = [];
+    let dayObject = {};
+
+    for (const element of state.days) {
+      if (element.appointments.includes(id)) {
+        //Figure out which day the input appointment is on (using the appt id)
+        dayName = element.name
+        //Get an array that contains all the appointment ids for that day
+        apptArray = [...element.appointments]
+        dayObject = { ...element };
+      }
+    }
+    // console.log('dayObject', dayObject)
+
+    //they're in order, so maybe you could do this with math instead
+    //Grab the appointment details from state for each other appts in the day's array
+    for (const element of apptArray) {
+      if (element === state.appointments[element].id) {
+        spotsArray.push(state.appointments[element].interview)
+      }
+    }
+
+    //If the appointment details are null, it means there's no appointment booked, so get a count of how many nulls are in the day
+    const spotsRemaining = spotsArray.filter(interview => interview === null).length;
+
+    // const updatedDay = {
+    //   ...dayObject,
+    //   spots: spotsRemaining
+    // }
+    // console.log('updatedDayObject', updatedDay)
+
+    // const updatedDays = [...state.days]
+
+    // console.log('updatedDays before', updatedDays)
+    // //use the numbers minus 1 to get the array index
+    // updatedDays[id - 1] = updatedDay;
+
+    // console.log('updatedDays after', updatedDays)
+
+    console.log('state', state)
+    return spotsRemaining;
+  };
+
   //interview manipulation functions
   const bookInterview = function (id, interview) {
     // console.log('id:', id, 'interview:', interview);
@@ -47,6 +95,7 @@ const useApplicationHook = function () {
 
     return axios.put(`/api/appointments/${id}`, { interview })
       .then(resolve => {
+        updateSpots(1);
         setState({
           ...state,
           appointments
