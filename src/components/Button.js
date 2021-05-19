d053cb95f12c3fd1455d8b9b63f9af4ee6657b2c
unknown <briannac@shaw.ca>
2021-05-19 20:25:59 +0100

    cleanup and remove console logs

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,10 @@ import classNames from 'classnames'
 import "components/Button.scss";
 
 export default function Button(props) {
+  //Conditional styling
   let buttonClass = classNames("button", { "button--confirm": props.confirm }, { "button--danger": props.danger });
 
-
+  //Button component
   return (
     <button
       onClick={props.onClick}
