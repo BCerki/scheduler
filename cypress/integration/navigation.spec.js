fa9ff942b594664e2d21f7f4db532cb4303925fd
unknown <briannac@shaw.ca>
2021-05-19 21:33:18 +0100

    install cypress and write should navigate to Tuesday test

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,17 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    // cy.visit("/")
+    // cy.get("li")
+    //   .contains("Tuesday")
+
+
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
+
+});
