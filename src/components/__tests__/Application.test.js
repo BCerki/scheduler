0be1ca86f642d4f665f92a343b425459a805bfc7
unknown <briannac@shaw.ca>
2021-05-19 03:48:43 +0100

    write test for editing interview per The Next Tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -80,10 +80,46 @@ describe("Application", () => {
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
 
-  it("loads data, edits an interview and keeps the spots remaining for Monday the same", () => {
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
 
-  })
+    // 3. Click the "Edit" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    // 4. Check that the form is shown--actually, this is covered by next step
+
+
+    //5. Change the student name and interviewer
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Fred Penner" }
+    });
+
+
+    // 6. Click the "Save" button on the confirmation.
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // 7. Check that the element with the text "Saving..." is displayed.
+    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
+
+
+    // 8. Wait until the updated element with the new student and interviewer name is displayed.
+    await waitForElement(() => getByText(appointment, "Fred Penner"));
+
+    // 9. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"))
+
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+  });
+
+
 
 
 });
